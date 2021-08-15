66823cbde16045b1e1539a5581d26494cf628a9c
Bryant Cassady <bryant.cassady@gmail.com>
2021-08-15 17:32:16 -0500

    Added Auto Example

---
+++ AutonomousExample.java
@@ -0,0 +1,20 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+@Autonomous
+public class DriveForward extends LinearOpMode {
+
+    // Declaration goes here
+
+    public void runOpMode() throws InterruptedException {
+        
+        // Initialization goes here
+
+        waitForStart();
+        
+        // Actions go here
+    }
+}
