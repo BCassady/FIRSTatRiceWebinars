5a05bdc1c615033743d2a9ce09408898aa2d654a
Bryant Cassady <bryant.cassady@gmail.com>
2021-09-02 22:38:55 -0500

    Added Turning Function

--- Session3/FunctionExample.java
+++ Session3/FunctionExample.java
@@ -1,8 +1,17 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.hardware.bosch.BNO055IMU;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.robotcore.external.navigation.Acceleration;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
+
+import java.util.Locale;
 
 public class FunctionExample extends LinearOpMode {
 
@@ -13,6 +22,12 @@ public class FunctionExample extends LinearOpMode {
 
     ElapsedTime timer;
 
+    Acceleration gravity;
+    BNO055IMU imu;
+    Orientation angles;
+    double angle;
+    String angleVal;
+
     @Override
     public void runOpMode() throws InterruptedException {
 
@@ -21,7 +36,17 @@ public class FunctionExample extends LinearOpMode {
         leftBack = hardwareMap.dcMotor.get("LB");
         rightBack = hardwareMap.dcMotor.get("RB");
 
+        final BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
+        parameters.angleUnit = BNO055IMU.AngleUnit.DEGREES;
+        parameters.accelUnit = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
+        parameters.calibrationDataFile = "BNO055IMUCalibration.json";
+        parameters.loggingEnabled = true;
+        parameters.loggingTag = "IMU";
+        imu = hardwareMap.get(BNO055IMU.class, "imu");
+        imu.initialize(parameters);
+
         timer = new ElapsedTime();
+
         waitForStart();
 
     }
@@ -53,4 +78,43 @@ public class FunctionExample extends LinearOpMode {
         rightFront.setPower(0.0);
     }
 
+    void pointTurn(final char lr, final double targetAngle, final double motorPower) {
+        if (lr == 'l') {
+            while (angle < targetAngle && opModeIsActive()) {
+                angles = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
+                angleVal = formatAngle(angles.angleUnit, angles.firstAngle);
+                angle = Float.parseFloat(angleVal);
+                if (targetAngle == 180.0 && angle < 0.0) {
+                    final double dif = 180.0 - Math.abs(angle);
+                    angle = 180.0 + dif;
+                }
+                leftBack.setPower(-motorPower);
+                leftFront.setPower(-motorPower);
+                rightBack.setPower(-motorPower);
+                rightFront.setPower(-motorPower);
+            }
+        } else {
+            while (angle > targetAngle && opModeIsActive()) {
+                angles = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
+                angleVal = formatAngle(angles.angleUnit, angles.firstAngle);
+                angle = Float.parseFloat(angleVal);
+                leftBack.setPower(motorPower);
+                leftFront.setPower(motorPower);
+                rightBack.setPower(motorPower);
+                rightFront.setPower(motorPower);
+            }
+        }
+        leftBack.setPower(0.0);
+        leftFront.setPower(0.0);
+        rightBack.setPower(0.0);
+        rightFront.setPower(0.0);
+    }
+
+    String formatAngle(final AngleUnit angleUnit, final double angle) {
+        return formatDegrees(AngleUnit.DEGREES.fromUnit(angleUnit, angle));
+    }
+
+    String formatDegrees(final double degrees) {
+        return String.format(Locale.getDefault(), "%.1f", AngleUnit.DEGREES.normalize(degrees));
+    }
 }
